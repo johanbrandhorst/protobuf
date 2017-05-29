7bc141419848e947d9af8af7c197de1a22524678
Johan Brandhorst <johan@cognitivelogic.com>
2017-05-29 23:37:39 +0100

    Remove jspb and import from jspb instead

--- grpcwebjs/webpack.config.js
+++ grpcwebjs/webpack.config.js
@@ -2,13 +2,10 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: {
-    jspb: "./node_modules/google-protobuf/google-protobuf.js",
-    grpc: "./node_modules/grpc-web-client/dist/index.js",
-  },
+  entry: "./node_modules/grpc-web-client/dist/index.js",
   output: {
     path: path.resolve(__dirname),
-    filename: '[name].inc.js',
+    filename: 'grpc.inc.js',
     libraryTarget: 'this',
   },
   resolve: {
