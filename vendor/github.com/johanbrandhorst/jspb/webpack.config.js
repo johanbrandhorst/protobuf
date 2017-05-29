579d1bed43ada2966541a5c1330933bcb2a55cc3
Johan Brandhorst <johan@cognitivelogic.com>
2017-05-29 23:34:52 +0100

    Switch to jspb for protobufjs

--- vendor/github.com/johanbrandhorst/jspb/webpack.config.js
+++ vendor/github.com/johanbrandhorst/jspb/webpack.config.js
@@ -2,13 +2,10 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: {
-    jspb: "./node_modules/google-protobuf/google-protobuf.js",
-    grpc: "./node_modules/grpc-web-client/dist/index.js",
-  },
+  entry:  "./node_modules/google-protobuf/google-protobuf.js",
   output: {
     path: path.resolve(__dirname),
-    filename: '[name].inc.js',
+    filename: 'jspb.inc.js',
     libraryTarget: 'this',
   },
   resolve: {
