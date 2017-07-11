22a2646d9c1a660d4a3cba75a2e4befd0beeebe7
Johan Brandhorst <johan@cognitivelogic.com>
2017-07-11 20:58:20 +0100

    Add some fancy pants error parsing to tests

--- test/webdriver_test.go
+++ test/webdriver_test.go
@@ -1,9 +1,13 @@
 package webdriver_test
 
 import (
+	"fmt"
+	"strings"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/sclevine/agouti"
+	"github.com/sclevine/agouti/api"
 	. "github.com/sclevine/agouti/matchers"
 )
 
@@ -27,8 +31,77 @@ var _ = Describe("gRPC-Web Unit Tests", func() {
 		})
 
 		By("finding the number of failures", func() {
-			Eventually(page.FindByClass("failed")).Should(BeFound())
-			Expect(page.FindByClass("failed")).To(HaveText("0"))
+			Eventually(page.FirstByClass("failed")).Should(BeFound())
+			failures, err := page.FindByID("qunit-testresult").FindByClass("failed").Text()
+			Expect(err).NotTo(HaveOccurred())
+			if failures == "0" {
+				return
+			}
+
+			// We have at least one failure - lets compile an error message
+			Eventually(page.FindByID(
+				"qunit-tests",
+			).AllByClass(
+				"fail",
+			).AllByClass(
+				"fail",
+			)).Should(BeFound())
+			messages := page.FindByID(
+				"qunit-tests",
+			).AllByClass(
+				"fail",
+			).AllByClass(
+				"fail",
+			)
+			elements, err := messages.Elements()
+			Expect(err).NotTo(HaveOccurred())
+			var errMsgs []string
+			for _, element := range elements {
+				// Get error summary
+				msg, err := element.GetElement(api.Selector{
+					Using: "css selector",
+					Value: ".test-message",
+				})
+				Expect(err).NotTo(HaveOccurred())
+				errText, err := msg.GetText()
+				Expect(err).NotTo(HaveOccurred())
+
+				// Get diff
+				expected, err := element.GetElements(api.Selector{
+					Using: "css selector",
+					Value: "del",
+				})
+				Expect(err).NotTo(HaveOccurred())
+				var expectedText string
+				if len(expected) > 0 {
+					expectedText, err = expected[0].GetText()
+					Expect(err).NotTo(HaveOccurred())
+				}
+				actual, err := element.GetElements(api.Selector{
+					Using: "css selector",
+					Value: "ins",
+				})
+				Expect(err).NotTo(HaveOccurred())
+				var actualText string
+				if len(actual) > 0 {
+					actualText, err = actual[0].GetText()
+					Expect(err).NotTo(HaveOccurred())
+				}
+
+				errMsg := errText
+				if expectedText != "" && actualText != "" {
+					errMsg = fmt.Sprintf(
+						"%s\n\tExpected: %q\n\tActual: %q",
+						errText,
+						strings.TrimSuffix(expectedText, " "),
+						strings.TrimSuffix(actualText, " "),
+					)
+				}
+				errMsgs = append(errMsgs, errMsg)
+			}
+
+			// Prints each error
+			Fail(strings.Join(errMsgs, "\n-----------------------------------\n"))
 		})
 	})
 })
