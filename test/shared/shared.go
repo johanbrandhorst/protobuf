0e4fb9049bdeb91a296bd6c858f0935e77035d05
Johan Brandhorst <johan@cognitivelogic.com>
2017-07-14 23:34:36 +0100

    Add shared file between test client and server

---
+++ test/shared/shared.go
@@ -0,0 +1,18 @@
+package shared
+
+// Addresses for the various servers
+const (
+	HTTP1Server           = "localhost:9090"
+	EmptyHTTP1Server      = "localhost:9095"
+	HTTP2Server           = "localhost:9100"
+	EmptyHTTP2Server      = "localhost:9105"
+	GopherJSServer        = "localhost:10000"
+	ClientMDTestKey       = "HeaderTestKey1"
+	ClientMDTestValue     = "ClientValue1"
+	ServerMDTestKey1      = "HeaderTestKey1"
+	ServerTrailerTestKey1 = "TrailerTestKey1"
+	ServerMDTestValue1    = "ServerValue1"
+	ServerTrailerTestKey2 = "TrailerTestKey2"
+	ServerMDTestKey2      = "HeaderTestKey2"
+	ServerMDTestValue2    = "ServerValue2"
+)
