17192d4c1319852148d575b002786bec9f1e08c1
Johan Brandhorst <johan@cognitivelogic.com>
2017-08-09 21:48:31 +0100

    Significantly simplify protoc-gen-gopherjs test generation with webpack

--- protoc-gen-gopherjs/test/types/types_pb.inc.js
+++ protoc-gen-gopherjs/test/types/types_pb.inc.js
@@ -1,3 +1,72 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
 /**
  * @fileoverview
  * @enhanceable
@@ -5,11 +74,11 @@
  */
 // GENERATED CODE -- DO NOT EDIT!
 
-var jspb = $global;
+var jspb = __webpack_require__(1);
 var goog = jspb;
 var global = Function('return this')();
 
-var multi_multi1_pb = $global.proto.multitest;
+var multi_multi1_pb = __webpack_require__(2);
 goog.exportSymbol('proto.types.ForeignEnum', null, global);
 goog.exportSymbol('proto.types.ForeignMessage', null, global);
 goog.exportSymbol('proto.types.MapEnum', null, global);
@@ -3125,4 +3194,20 @@ proto.types.MapEnum = {
   MAP_ENUM_BAZ: 2
 };
 
+goog.object.extend(exports, proto.types);
 
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+module.exports = window;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+module.exports = window.proto.multitest;
+
+/***/ })
+/******/ ]);
